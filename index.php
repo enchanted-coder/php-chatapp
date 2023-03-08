3c2e29b9a3077d5c2cbf654c94feb4c1446c0ebe
enchanted-coder <danieltogey@pm.me>
2023-03-08 20:17:09 +0300

    index.php now has a few files. html code to check we're in index and a php session variable

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+	$_SESSION;
+	
+?>
+
+<html land="en">
+	<head>
+		<title>ChatR</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="" rel="stylesheet">
+	</head>
+	<body>
+
+		<button><a href="logout.php">Logout here</a></button>
+		<h1>This is the index page.</h1>
+
+		<br>
+		Hello, Danny.
+	</body>
+</html>
