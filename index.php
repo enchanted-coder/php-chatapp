ef97e4e7cd1308d6f3a1a1bc2bb408fd3792cba8
enchanted-coder <danieltogey@pm.me>
2023-03-09 07:01:15 +0300

    added a logout session

--- index.php
+++ index.php
@@ -2,6 +2,31 @@
 
 session_start();
 
-print_r($_SESSION);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Home</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" rel="stylesheet">
+  </head>
+  <body>
+		<h1>Home</h1>
+
+		<?php if(isset($_SESSION["user_id"])): ?>
+
+			<p>You are logged in</p>
+
+			<p><a href="logout.php">Log out</a></p>
+
+		<?php else: ?>
+
+			<p><a href="login.php">Log in</a> or <a href="signup.html">Sign up</a></p>
+
+		<?php endif; ?>
+
+  </body>
+</html>
