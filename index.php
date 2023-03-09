a4e8f827fb79ee7dc4f3673dd76d38c3d2843dd4
enchanted-coder <danieltogey@pm.me>
2023-03-09 07:18:57 +0300

    ive made a perfect login system. regenerates session i can stop here and get some sleep

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 session_start();
 
+if(isset($_SESSION["user_id"])) {
+
+	$mysqli = require __DIR__ . "/database.php";
+
+	$sql = "SELECT * FROM user
+			WHERE id = {$_SESSION["user_id"]}";
+
+	$result = $mysqli->query($sql);
+
+	$user = $result->fetch_assoc();
+}
 
 ?>
 
@@ -16,9 +27,9 @@ session_start();
   <body>
 		<h1>Home</h1>
 
-		<?php if(isset($_SESSION["user_id"])): ?>
+		<?php if (isset($user)): ?>
 
-			<p>You are logged in</p>
+			<p>Hello <?= htmlspecialchars($user["name"])?></p>
 
 			<p><a href="logout.php">Log out</a></p>
 
--- login.php
+++ login.php
@@ -18,6 +18,8 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
 
 			session_start();
 
+			session_regenerate_id();
+
 			$_SESSION["user_id"] = $user["id"];
 			header("Location: index.php");
 			exit;
