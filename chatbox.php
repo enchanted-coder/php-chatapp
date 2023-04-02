6f3307bc1dfd715ffd684fe0b2f31df7d2a4910b
enchanted-coder <danieltogey@pm.me>
2023-04-02 20:56:42 +0300

    js validation plus chatbox inside js directory

--- chatbox.php
+++ chatbox.php
@@ -1,219 +1,50 @@
 <?php
 if (isset($_POST['submit'])){
 
-$link = mysqli_connect("localhost",
-			"root", "", "chatr");
+  $mysqli = require __DIR__ . "./database.php"; 
 
-// Check connection
-if($link === false){
-	die("ERROR: Could not connect. "
-		. mysqli_connect_error());
-}
 
-// Escape user inputs for security
 $username= mysqli_real_escape_string(
-	$link, $_REQUEST['username']);
+	$mysqli, $_REQUEST['username']);
 $message = mysqli_real_escape_string(
-	$link, $_REQUEST['message']);
+	$mysqli, $_REQUEST['message']);
 date_default_timezone_set('Africa/Nairobi');
 $timestamp=date('y-m-d h:ia');
 
-// Attempt insert query execution
 $sql = "INSERT INTO chats (username, message, timestamp)
 		VALUES ('$username', '$message', '$timestamp')";
-if(mysqli_query($link, $sql)){
+if(mysqli_query($mysqli, $sql)){
 	;
 } else{
 	echo "ERROR: Message not sent!!!";
 }
-// Close connection
-mysqli_close($link);
+mysqli_close($mysqli);
 }
 ?>
-<html>
+
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<style>
-*{
-	box-sizing:border-box;
-}
-body{
-	background-color:antiquewhite;
-	font-family:Arial;
-}
-#container{
-	width:500px;
-	height:700px;
-	background:white;
-	margin:0 auto;
-	font-size:0;
-	border-radius:5px;
-	overflow:hidden;
-}
-main{
-	width:500px;
-	height:700px;
-	display:inline-block;
-	font-size:15px;
-	vertical-align:top;
-}
-main header{
-	height:100px;
-	padding:30px 20px 30px 40px;
-	background-color:gray;
-}
-main header > *{
-	display:inline-block;
-	vertical-align:top;
-}
-main header img:first-child{
-	width:24px;
-	margin-top:8px;
-}
-main header img:last-child{
-	width:24px;
-	margin-top:8px;
-}
-main header div{
-	margin-left:90px;
-	margin-right:90px;
-}
-main header h2{
-	font-size:25px;
-	margin-top:5px;
-	text-align:center;
-	color:#FFFFFF;
-}
-main .inner_div{
-	padding-left:0;
-	margin:0;
-	list-style-type:none;
-	position:relative;
-	overflow:auto;
-	height:500px;
-	background-position:center;
-	background-repeat:no-repeat;
-	background-size:cover;
-	position: relative;
-	border-top:2px solid #fff;
-	border-bottom:2px solid #fff;
-	
-}
-main .triangle{
-	width: 0;
-	height: 0;
-	border-style: solid;
-	border-width: 0 8px 8px 8px;
-	border-color: transparent transparent
-	#58b666 transparent;
-	margin-left:20px;
-	clear:both;
-}
-main .message{
-	padding:10px;
-	color:#000;
-	margin-left:15px;
-	background-color:#58b666;
-	line-height:20px;
-	max-width:90%;
-	display:inline-block;
-	text-align:left;
-	border-radius:5px;
-	clear:both;
-}
-main .triangle1{
-	width: 0;
-	height: 0;
-	border-style: solid;
-	border-width: 0 8px 8px 8px;
-	border-color: transparent
-	transparent #6fbced transparent;
-	margin-right:20px;
-	float:right;
-	clear:both;
-}
-main .message1{
-	padding:10px;
-	color:#000;
-	margin-right:15px;
-	background-color:#6fbced;
-	line-height:20px;
-	max-width:90%;
-	display:inline-block;
-	text-align:left;
-	border-radius:5px;
-	float:right;
-	clear:both;
-}
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/x-icon" href="./assets/chat-bubbles.png">
+    <link href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" rel="stylesheet">
+	<script src="./js/chatbox.js" defer ></script>
+    <title>chat.chatr</title>
+</head>
 
-main footer{
-	height:150px;
-	padding:20px 30px 10px 20px;
-	background-color:gray;
-}
-main footer .input1{
-	resize:none;
-	border:100%;
-	display:block;
-	width:120%;
-	height:55px;
-	border-radius:3px;
-	padding:20px;
-	font-size:13px;
-	margin-bottom:13px;
-}
-main footer textarea{
-	resize:none;
-	border:100%;
-	display:block;
-	width:140%;
-	height:55px;
-	border-radius:3px;
-	padding:20px;
-	font-size:13px;
-	margin-bottom:13px;
-	margin-left:20px;
-}
-main footer .input2{
-	resize:none;
-	border:100%;
-	display:block;
-	width:40%;
-	height:55px;
-	border-radius:3px;
-	padding:20px;
-	font-size:13px;
-	margin-bottom:13px;
-	margin-left:100px;
-	color:white;
-	text-align:center;
-	background-color:black;
-	border: 2px solid white;
-}
-}
-main footer textarea::placeholder{
-	color:#ddd;
-}
-
-</style>
 <body onload="show_func()">
 <div id="container">
 	<main>
 		<header>
-			<img src="https://s3-us-west-2.amazonaws.com/
-			s.cdpn.io/1940306/ico_star.png" alt="">
 			<div>
 				<h2>CHATBOX</h2>
 			</div>
 		</header>
 
-<script>
-function show_func(){
 
-var element = document.getElementById("chathist");
-	element.scrollTop = element.scrollHeight;
-
-}
-</script>
 
 <form id="myform" action="Group_chat.php" method="POST" >
 <div class="inner_div" id="chathist">
@@ -240,8 +71,8 @@ $first=$row;
 <div>
 <span style="color:black;float:left;
 font-size:10px;clear:both;">
-	<?php echo $row['uname']; ?>,
-		<?php echo $row['dt']; ?>
+	<?php echo $row['username']; ?>,
+		<?php echo $row['timestamp']; ?>
 </span>
 </div>
 </div>
---
+++ js/chatbox.js
@@ -0,0 +1,6 @@
+function show_func(){
+
+    var element = document.getElementById("chathist");
+        element.scrollTop = element.scrollHeight;
+    
+    }
