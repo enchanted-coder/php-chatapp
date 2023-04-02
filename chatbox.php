1ef2570f407d40d6717eeadacad93d4fffa2d123
enchanted-coder <danieltogey@pm.me>
2023-04-03 00:05:06 +0300

    ok so this was hiding in plain sight hehe so i dont actually need a bloody table or even a database for our chat app all i need is websockets so figure out how to implement this in php and the different messages will be from users in our database this will function as an amnesia chat of some sort since no data about the chat will be stored in the database. just the browser memory so this should be wiped out the momemnt the browser refreshes oh yea and new clients will only have their memory so they wont be able to see earlier chats this is a FEATURE and totally not because im too lazy yep so time to burn the next 4 hours figuring websockets out

--- chatbox.php
+++ chatbox.php
@@ -22,7 +22,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon" type="image/x-icon" href="./assets/chat-bubbles.png">
     <link href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" rel="stylesheet">
-	<script src="./js/chatbox.js" defer ></script>
+	
     <title>chat.chatr</title>
 </head>
 
@@ -40,104 +40,7 @@
 
 <form id="myform" action="Group_chat.php" method="POST" >
 <div class="inner_div" id="chathist">
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db_name = "chatr";
-$con = new mysqli($host, $user, $pass, $db_name);
 
-$query = "SELECT * FROM chats";
-$run = $con->query($query);
-$i=0;
-
-while($row = $run->fetch_array()) :
-if($i==0){
-$i=5;
-$first=$row;
-?>
-<div id="triangle1" class="triangle1"></div>
-<div id="message1" class="message1">
-<span style="color:white;float:right;">
-<?php echo $row['msg']; ?></span> <br/>
-<div>
-<span style="color:black;float:left;
-font-size:10px;clear:both;">
-	<?php echo $row['username']; ?>,
-		<?php echo $row['timestamp']; ?>
-</span>
-</div>
-</div>
-<br/><br/>
-<?php
-}
-else
-{
-if($row['username']!=$first['username'])
-{
-?>
-<div id="triangle" class="triangle"></div>
-<div id="message" class="message">
-<span style="color:white;float:left;">
-<?php echo $row['message']; ?>
-</span> <br/>
-<div>
-<span style="color:black;float:right;
-		font-size:10px;clear:both;">
-<?php echo $row['username']; ?>,
-		<?php echo $row['timestamp']; ?>
-</span>
-</div>
-</div>
-<br/><br/>
-<?php
-}
-else
-{
-?>
-<div id="triangle1" class="triangle1"></div>
-<div id="message1" class="message1">
-<span style="color:white;float:right;">
-<?php echo $row['message']; ?>
-</span> <br/>
-<div>
-<span style="color:black;float:left;
-		font-size:10px;clear:both;">
-<?php echo $row['username']; ?>,
-	<?php echo $row['datetime']; ?>
-</span>
-</div>
-</div>
-<br/><br/>
-<?php
-}
-}
-endwhile;
-?>
-</div>
-	<footer>
-		<table>
-		<tr>
-		<th>
-			<input class="input1" type="text"
-					id="username" name="username"
-					placeholder="From">
-		</th>
-		<th>
-			<textarea id="message" name="message"
-				rows='3' cols='50'
-				placeholder="Type your message">
-			</textarea></th>
-		<th>
-			<input class="input2" type="submit"
-			id="submit" name="submit" value="send">
-		</th>			
-		</tr>
-		</table>			
-	</footer>
-</form>
-</main>
-</div>
 
 </body>
 </html>
--- js/chatbox.js
+++
@@ -1,6 +0,0 @@
-function show_func(){
-
-    var element = document.getElementById("chathist");
-        element.scrollTop = element.scrollHeight;
-    
-    }
