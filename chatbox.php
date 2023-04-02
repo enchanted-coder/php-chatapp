b4d50d4e6e23c11ce5661a3759b1fe2c8f049eb3
enchanted-coder <danieltogey@pm.me>
2023-04-02 23:20:57 +0300

    returned the chat tables removed in previous commit

--- chatbox.php
+++ chatbox.php
@@ -40,7 +40,80 @@
 
 <form id="myform" action="Group_chat.php" method="POST" >
 <div class="inner_div" id="chathist">
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db_name = "chatr";
+$con = new mysqli($host, $user, $pass, $db_name);
 
+$query = "SELECT * FROM chats";
+$run = $con->query($query);
+$i=0;
+
+while($row = $run->fetch_array()) :
+if($i==0){
+$i=5;
+$first=$row;
+?>
+<div id="triangle1" class="triangle1"></div>
+<div id="message1" class="message1">
+<span style="color:white;float:right;">
+<?php echo $row['msg']; ?></span> <br/>
+<div>
+<span style="color:black;float:left;
+font-size:10px;clear:both;">
+	<?php echo $row['username']; ?>,
+		<?php echo $row['timestamp']; ?>
+</span>
+</div>
+</div>
+<br/><br/>
+<?php
+}
+else
+{
+if($row['username']!=$first['username'])
+{
+?>
+<div id="triangle" class="triangle"></div>
+<div id="message" class="message">
+<span style="color:white;float:left;">
+<?php echo $row['message']; ?>
+</span> <br/>
+<div>
+<span style="color:black;float:right;
+		font-size:10px;clear:both;">
+<?php echo $row['username']; ?>,
+		<?php echo $row['timestamp']; ?>
+</span>
+</div>
+</div>
+<br/><br/>
+<?php
+}
+else
+{
+?>
+<div id="triangle1" class="triangle1"></div>
+<div id="message1" class="message1">
+<span style="color:white;float:right;">
+<?php echo $row['message']; ?>
+</span> <br/>
+<div>
+<span style="color:black;float:left;
+		font-size:10px;clear:both;">
+<?php echo $row['username']; ?>,
+	<?php echo $row['datetime']; ?>
+</span>
+</div>
+</div>
+<br/><br/>
+<?php
+}
+}
+endwhile;
+?>
 </div>
 	<footer>
 		<table>
